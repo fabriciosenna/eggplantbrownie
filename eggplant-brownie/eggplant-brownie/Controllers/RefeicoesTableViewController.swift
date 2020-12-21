4dc6b77ee8ff80f34aa2f6f09155865a2ebb518f
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-21 14:08:24 -0300

    Eventos com longpress

--- eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -41,7 +41,11 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
     
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer){
         if gesture.state == .began{
-            print("longPress gesture")
+            let celula = gesture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else{return}
+            let refeicao = refeicoes[indexPath.row]
+            
+            print("refeicao: \(refeicao.nome)")
         }
     }
     
