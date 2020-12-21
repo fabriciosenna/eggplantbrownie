3f7dc0df7fc47c2d58119b9ac004d7e8378ce6d5
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-21 11:16:52 -0300

    Invocando um delegate, validação de optionals usando guard

--- eggplant-brownie/eggplant-brownie/Controllers/AdicionarItensViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class AdicionarItensViewController: UIViewController {
 
+    // MARk: - IBOutlets
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var caloriasTextField: UITextField!
+    
+    
     // MARK: - View life cycle
     
     override func viewDidLoad() {
@@ -21,6 +27,14 @@ class AdicionarItensViewController: UIViewController {
     // MARK: - IBAction
     
     @IBAction func adicionarItem(_ sender: Any) {
+        
+        guard let nome = nomeTextField.text, let calorias = caloriasTextField.text else{
+            return
+        }
+        
+        if let numeroDeCalorias = Double(calorias){
+            let item = Item(nome: nome, calorias: numeroDeCalorias)
+        }
         navigationController?.popViewController(animated: true)
     }
     
