fcf21bb18cc9c377dbbc62b008bf4b72b08aa05e
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-21 22:53:37 -0300

    Refatoração

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -90,19 +90,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // MARK: - IBActions
     
-    @IBAction func adicionar(_ sender: Any) {
-        
+    func recuperaRefeicaoDoFormulario() -> Refeicao?{
         guard let nomeDaRefeicao = nomeTextField?.text else {
-            return
+            return nil
         }
         
         guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
-            return
+            return nil
         }
         
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
         
-        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        return refeicao
+    }
+    
+    @IBAction func adicionar(_ sender: Any) {
+        
+        guard let refeicao = recuperaRefeicaoDoFormulario() else{
+            Alerta(controller: self).exibe(mensagem: "Erro ao ler dados do formulário")
+            return
+        }
         
         delegate?.add(refeicao)
         navigationController?.popViewController(animated: true)
